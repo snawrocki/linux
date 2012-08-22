637857c7cd5f3b4ad1a302c96b1ec0e96d553afa
Sylwester Nawrocki <sylvester.nawrocki@gmail.com>
2012-08-23 01:14:23 +0200

    V4L: Add OV9650 image sensor V4L2 sub-device driver
     - Add reset delay
     - Enable AE, AWB, AG
     - remove broken streaming ref counting
     - remove regulator support
     - move register definitions to .c file
     - add functions for gamma curve and color matrix set up
     - resolution setting WIP
     - add missing initialization of the pixel format register
     - enable debug
    
    Signed-off-by: Sylwester Nawrocki <sylvester.nawrocki@gmail.com>

---
+++ include/media/ov9650.h
@@ -0,0 +1,20 @@
+/*
+ * OV9650/OV9652 camera sensors driver
+ *
+ * Copyright (C) 2012 Sylwester Nawrocki <sylvester.nawrocki@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef OV9650_H_
+#define OV9650_H_
+
+struct ov9650_platform_data {
+	unsigned long mclk_frequency;
+	int gpio_pwdn;
+	int gpio_reset;
+};
+
+#endif /* OV9650_H_ */
